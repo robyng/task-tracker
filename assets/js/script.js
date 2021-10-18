4472b0349f4f49b16a28b5c69b4736d4a29cc20f
robyng <robyngraham01@gmail.com>
2021-10-18 16:18:03 -0700

    innerHTML for select dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,26 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
- var createTaskHandler = function(event) {
-
+var createTaskHandler = function(event) {
   event.preventDefault()
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+   
+// create list item
+var listItemEl = document.createElement("li");
+listItemEl.className = "task-item";
 
-   var listItemEl = document.createElement("li");
-   listItemEl.className = "task-item";
-   listItemEl.textContent = "this is new task";
-   tasksToDoEl.appendChild(listItemEl);
+// create div to hold task info and add to list item
+var taskInfoEl = document.createElement("div");
+taskInfoEl.className = "task-info";
+
+// add HTML content to div
+taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+listItemEl.appendChild(taskInfoEl);
+
+// add entire list item to list
+tasksToDoEl.appendChild(listItemEl);
+
  };
 
 formEl.addEventListener("submit", createTaskHandler)
